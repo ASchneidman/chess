746f9486766772f5a1689064d0920ebe85c66e04
Alex Schneidman <alexmarkschneidman@gmail.com>
2024-08-16 10:39:51 -0700

    chess vec 2d

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
+fn print_state(values: &mut Vec<Vec<&str>>) {
+    for x in 0..7 {
+        for y in 0..7 {
+            print!("{} ", values[x][y]);
+        }
+        println!();
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    // pawn, rook, knight, bishop, queen, king
+    // 6 is the largest name, so everything needs to be 6
+    let mut values = vec![vec!["______"; 8]; 8];
+    values[0][0] = "hmm";
+    print_state(&mut values);
 }
